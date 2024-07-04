d06d74ac8c4c7bcbc71e5a42301cbf7081c72a06
wb_huangzhihao02 <wb_hunagzhihao02@meituan.com>
2024-07-04 09:36:17 +0800

    初始化修改

---
+++ CobraPlatform/Applaunch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CobraPlatform
+//
+//  Created by mac on 2024/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
